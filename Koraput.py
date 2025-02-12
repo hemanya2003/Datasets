6864098070041b8602a082ee5fbd20c2a77751a5
hemanya2003 <hemanyacherry@gmail.com>
2025-02-12 20:09:52 +0530

    Update Koraput.py

--- Koraput.py
+++ Koraput.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNL3Oc01K8MMrSMQhX/bwes",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
